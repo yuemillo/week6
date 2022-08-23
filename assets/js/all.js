d5a8e5dba8559e5357d805a43ecbc83c449765d1
yuemillo <yc.gd1688@gmail.com>
2022-08-24 01:29:52 +0800

    Update 2022-08-23T17:29:47.916Z

---
+++ assets/js/all.js
@@ -0,0 +1,13 @@
+"use strict";
+
+$(function () {
+  console.log('Hello Bootstrap5');
+});
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 8,
+  autoplay: {
+    delay: 5000
+  }
+});
+//# sourceMappingURL=all.js.map
